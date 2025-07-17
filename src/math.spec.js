cf10746d38620752c1c4c30d5e5fd93d1872118a
ChristosBaboulis <chrisb2603@gmail.com>
2025-07-17 19:24:09 +0300

    Unit tests progress

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,11 +1,13 @@
 import { isEven } from './math';
 
-it('isEven should return true if given an even number', () => {
-  isEven(2);
-  expect(isEven(2)).toBe(true);
-});
+describe('isEven', () => {
+  it('Should return true if given an even number', () => {
+    isEven(2);
+    expect(isEven(2)).toBe(true);
+  });
 
-it('isEven should return false if given an odd number', () => {
-  isEven(1);
-  expect(isEven(1)).toBe(false);
+  it('Should return false if given an odd number', () => {
+    isEven(1);
+    expect(isEven(1)).toBe(false);
+  });
 });

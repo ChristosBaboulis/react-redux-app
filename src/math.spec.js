b72888363b360f529efb3259940a95240f8b23d3
ChristosBaboulis <chrisb2603@gmail.com>
2025-07-17 19:08:40 +0300

    Added 2nd test

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,6 +1,11 @@
 import { isEven } from './math';
 
-it('first test', () => {
+it('isEven should return true if given an even number', () => {
   isEven(2);
   expect(isEven(2)).toBe(true);
+});
+
+it('isEven should return false if given an odd number', () => {
+  isEven(1);
+  expect(isEven(1)).toBe(false);
 });

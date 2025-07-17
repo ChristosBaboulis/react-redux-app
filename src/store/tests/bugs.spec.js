09fe01720ea7e3b1e1c4dd07eed0144ff2db642a
ChristosBaboulis <chrisb2603@gmail.com>
2025-07-17 21:10:19 +0300

    Changed integration test to unit test

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,11 +1,18 @@
 import configureStore from '../configureStore';
 import { addBug } from '../bugs';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
 
 describe('bugslice', () => {
   it('should handle the addBug action', async () => {
-    const store = configureStore();
     const bug = { description: 'a' };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost('/bugs').reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
